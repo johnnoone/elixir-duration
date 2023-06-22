19fd95fc8937ee061b3e1e9fa9f221e3e81b8d61
Simon de Haan <simon@turn.io>
2023-06-22 17:52:18 +0200

    update deps & address compilation issues

--- mix.exs
+++ mix.exs
@@ -11,8 +11,8 @@ defmodule Duration.MixProject do
       package: package(),
       deps: deps(),
       name: "Duration",
-      source_url: "https://lab.errorist.xyz/elixir/duration",
-      homepage_url: "http://elixir.errorist.io/duration",
+      source_url: "https://github.com/johnnoone/elixir-duration",
+      homepage_url: "https://github.com/johnnoone/elixir-duration",
       docs: docs()
     ]
   end
@@ -27,8 +27,8 @@ defmodule Duration.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:nimble_parsec, "~> 0.5.0", only: [:dev, :test]},
-      {:ex_doc, "~> 0.21", only: [:dev, :test], runtime: false}
+      {:nimble_parsec, "~> 1.3.1", only: [:dev, :test]},
+      {:ex_doc, "~> 0.29", only: [:dev, :test], runtime: false}
     ]
   end
 
@@ -49,10 +49,10 @@ defmodule Duration.MixProject do
       # main: "Duration", # The main page in the docs
       # logo: "path/to/logo.png",
       extras: [
-        "guides/advanced/timex.md",
+        "guides/advanced/timex.md"
       ],
       groups_for_extras: [
-        "Advanced": Path.wildcard("guides/advanced/*.md")
+        Advanced: Path.wildcard("guides/advanced/*.md")
       ]
     ]
   end
